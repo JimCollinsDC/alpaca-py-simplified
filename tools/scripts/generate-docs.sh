8fc9f9c7c0896127cf885e2122b91139b09df5e3
JamesCollins <james.collins@noaa.gov>
2025-11-09 20:30:10 -0500

    ci: update docs generation script to use uv instead of poetry

--- tools/scripts/generate-docs.sh
+++ tools/scripts/generate-docs.sh
@@ -8,8 +8,8 @@ pushd "$(dirname "$0")"/../../docs >>/dev/null
 set +e
 
 #clean any local files to prevent cached errors
-poetry run make clean
+uv run make clean
 
 #run make html with a flag to make sphinx treat warnings as errors instead of generating incomplete docs
 #we also run doctest to ensure any doctests are successful before generating html
-poetry run make html doctest SPHINXOPTS="-W"
+uv run make html doctest SPHINXOPTS="-W"
